7dd0f3d430edf4760f626cb9038f7e03d3e7d686
Nadia Islam <sohasara853@gmail.com>
2024-08-12 10:46:01 +0600

    modify

---
+++ lib/ui/view_task.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewTask extends StatelessWidget {
+  const ViewTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
