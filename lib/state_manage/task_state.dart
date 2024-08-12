2b8729c58643397d03c145e0bbd4cbe99c079b33
Nadia Islam <sohasara853@gmail.com>
2024-08-12 11:07:26 +0600

    create notifier class

--- lib/state_manage/task_state.dart
+++ lib/state_manage/task_state.dart
@@ -1,5 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 class TaskState {
   final String task;
 
   TaskState({required this.task});
+}
+
+class TaskNotifier extends StateNotifier {
+  TaskNotifier(super.state);
 }
