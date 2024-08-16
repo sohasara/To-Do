9dae1c3a7e55d86a94f507614daf211c288f1c82
Nadia Islam <sohasara853@gmail.com>
2024-08-16 19:00:07 +0600

    create data class

--- lib/data/data_class.dart
+++ lib/data/data_class.dart
@@ -1,1 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:hive_flutter/hive_flutter.dart';
 
+@HiveType(typeId: 0)
+class Tasks {
+  @HiveField(0)
+  final String task;
+  Tasks({
+    required this.task,
+  });
+}
